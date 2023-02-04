a2d30a947c98a514a97febb105ec5d7e433d2364
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 14:09:48 -0600

    feat: :sparkles: axios error interceptor added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import { AxiosInterceptor } from '@/utils'
+
+AxiosInterceptor()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer icon theme='colored' />
+    </>
+  )
 }

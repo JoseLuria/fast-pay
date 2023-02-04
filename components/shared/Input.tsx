b15f7284cc0e6086ec57f8a57ca14cd54281ce25
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 00:34:22 -0600

    feat: :lipstick: error show on input component

--- components/shared/Input.tsx
+++ components/shared/Input.tsx
@@ -21,20 +21,23 @@ export const Input: FC<Props> = ({
   error,
   label
 }) => {
-  const styles = clsx(
-    'w-full py-4 px-5 bg-dark-gray border-2 duration-200 rounded placeholder:text-gray',
+  const labelStyles = clsx('flex justify-between mb-[10px]', error && 'text-red duration-200')
+
+  const inputStyles = clsx(
+    'w-full py-3 px-4 bg-dark-gray border-2 duration-200 rounded placeholder:text-gray',
     error ? 'border-red focus-visible:border-red' : 'border-dark-gray focus-visible:border-gray',
     className
   )
 
   return (
-    <span className='text-sm'>
-      <label className='block mb-[10px]' htmlFor='email'>
+    <span className='text-xs w-full'>
+      <label className={labelStyles} htmlFor='email'>
         {label}
+        {error && <span>{error.message}</span>}
       </label>
       <input
         {...register(name)}
-        className={styles}
+        className={inputStyles}
         name={name}
         type={type}
         placeholder={placeholder}

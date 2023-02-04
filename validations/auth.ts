0587e83abb4397f2a46ae500cc16a5e525b053bb
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 17:36:22 -0600

    fix: :bug: changed validation name size on register

--- validations/auth.ts
+++ validations/auth.ts
@@ -7,7 +7,7 @@ export const loginSchema = z.object({
 })
 
 export const registerSchema = z.object({
-  name: z.string().min(6, errMsg.name),
+  name: z.string().min(2, errMsg.name),
   email: z.string().min(1, errMsg.empty).email(errMsg.email),
   password: z.string().min(6, errMsg.password)
 })

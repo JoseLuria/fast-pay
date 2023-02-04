9ce6d9fd8b8c29d6b022b15a0ed05d29b7b8af34
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 17:37:02 -0600

    fix: :lipstick: resolved react toastify styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 import type { FC } from 'react'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'

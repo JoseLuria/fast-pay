344bff8378fd2f8466aaa9e33a96082e6c77bcce
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 20:29:17 -0600

    feat: :sparkles: added session provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 import type { FC } from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { ToastContainer } from 'react-toastify'
 import { AxiosInterceptor } from '@/utils'
 
@@ -9,10 +10,10 @@ AxiosInterceptor()
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <SessionProvider>
       <Component {...pageProps} />
       <ToastContainer icon theme='colored' />
-    </>
+    </SessionProvider>
   )
 }
 

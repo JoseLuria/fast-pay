9a574677ac2d9bf23aa8b03123ef5762db4a1f03
JoseLuria <jos02-log18@hotmail.com>
2023-02-05 14:28:55 -0600

    feat: :lipstick: created invoice empty component

---
+++ components/invoices/InvoiceEmpty.tsx
@@ -0,0 +1,13 @@
+import { EmptyIllustration } from '@/components/icons'
+
+export const InvoiceEmpty = () => {
+  return (
+    <div className='mt-16 flex flex-col text-center items-center md:mt-20'>
+      <EmptyIllustration className='w-full max-w-[241px]' />
+      <h2 className='my-6 font-semibold text-xl'>Parece que no hay nada</h2>
+      <p className='max-w-[250px] text-sm text-white text-opacity-50'>
+        Cree una factura haciendo clic en el botón Nuevo y empiece a recibir pagos
+      </p>
+    </div>
+  )
+}

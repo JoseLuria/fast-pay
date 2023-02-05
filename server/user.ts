0beaf2352abcd53a56305bdf876b84a9a14b7b61
JoseLuria <jos02-log18@hotmail.com>
2023-02-04 20:25:16 -0600

    feat: :sparkles: created server auth function

---
+++ server/user.ts
@@ -0,0 +1,21 @@
+import bycript from 'bcryptjs'
+import { db } from '@/server'
+import { Login } from '@/validations'
+
+export const authUser = async ({ email, password }: Login) => {
+  const user = await db.user.findFirst({ where: { email } })
+
+  if (!user) {
+    return null
+  }
+
+  const isValidPassword = bycript.compareSync(password, user.password)
+
+  if (!isValidPassword) {
+    return null
+  }
+
+  const { id, name, image, type } = user
+
+  return { id, email, name, image, type }
+}

aa14fb057216983c3c19544e769ed46df20fad68
JoseLuria <jos02-log18@hotmail.com>
2023-02-05 15:31:27 -0600

    feat: :lipstick: changed app layout bottom

--- components/layouts/AppLayout.tsx
+++ components/layouts/AppLayout.tsx
@@ -12,7 +12,7 @@ export const AppLayout: FC<Props> = ({ children, title }) => {
       <Head title={title} />
       <AppBar />
 
-      <div className='w-full mt-24 p-6 md:p-12 lg:mt-0 lg:py-20 lg:ml-28 lg:w-auto lg:grow'>
+      <div className='w-full mt-24 px-6 py-6 pb-12 md:px-12 md:pt-12 md:pb-24 lg:mt-0 lg:py-20 lg:ml-28 lg:w-auto lg:grow'>
         <main className='w-full max-w-container mx-auto'>{children}</main>
       </div>
     </>

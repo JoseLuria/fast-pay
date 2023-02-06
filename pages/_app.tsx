8884427b13079967324b5582507c0ce779104609
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 09:46:01 -0600

    refactor: :zap: added toast container dynamic import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,12 @@ import 'react-toastify/dist/ReactToastify.min.css'
 import type { FC } from 'react'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
-import { ToastContainer } from 'react-toastify'
+import dynamic from 'next/dynamic'
 import { AxiosInterceptor } from '@/utils'
+
+const ToastContainer = dynamic(
+  import('react-toastify').then(({ ToastContainer }) => ToastContainer)
+)
 
 AxiosInterceptor()
 

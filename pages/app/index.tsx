6dd7a0a25f34c804726452d9e4f16a4e42cada3b
JoseLuria <jos02-log18@hotmail.com>
2023-02-06 09:48:59 -0600

    feat: :sparkles: added show user invoices on /app

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,25 +1,21 @@
-import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { AppLayout, InvoiceHeader, InvoiceLoading } from '@/components'
+import { useInvoices } from '@/hooks'
 
 const InvoiceEmpty = dynamic(import('@/components').then(({ InvoiceEmpty }) => InvoiceEmpty))
+const InvoiceList = dynamic(import('@/components').then(({ InvoiceList }) => InvoiceList))
 
 const Dashboard = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const invoices = []
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false)
-    }, 4000)
-
-    return () => clearTimeout(timer)
-  }, [])
+  const { invoices, isLoading } = useInvoices()
 
   return (
     <AppLayout title='Mis facturas'>
       <InvoiceHeader size={invoices.length} />
-      {isLoading ? <InvoiceLoading /> : <InvoiceEmpty />}
+      {isLoading ? (
+        <InvoiceLoading />
+      ) : (
+        <>{invoices.length > 0 ? <InvoiceList invoices={invoices} /> : <InvoiceEmpty />}</>
+      )}
     </AppLayout>
   )
 }

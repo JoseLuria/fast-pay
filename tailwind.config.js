204392149a20ece76c8e6d20f81d82d551cf9712
JoseLuria <jos02-log18@hotmail.com>
2023-04-04 13:47:06 -0500

    feat: :lipstick: added tailwind colors and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#ECEFF4',
+      black: '#131313',
+      'dark-gray': '#292929',
+      gray: '#949494',
+      green: '#33D69F',
+      orange: '#FF8F00',
+      red: '#E74C3C'
+    },
     extend: {
+      maxWidth: {
+        container: '730px',
+        auth: '450px'
+      },
+      minHeight: {
+        invoice: '134px',
+        'invoice-md': '72px'
+      },
       fontFamily: {
         poppins: '"Poppins", sans-serif'
       }

61bf44b610fec21981b01cfb2ee728ba914ca97f
JoseLuria <jos02-log18@hotmail.com>
2023-04-03 23:49:42 -0500

    feat: :lipstick: added poppins font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html lang='es'>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

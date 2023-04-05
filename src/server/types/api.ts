89a74343e0a2c0653ca38dc84cd0dfc80b17c3e2
JoseLuria <jos02-log18@hotmail.com>
2023-04-05 13:04:47 -0500

    feat: :label: added server types

---
+++ src/server/types/api.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest } from 'next/server'
+
+export type RequestSession = NextRequest | NextApiRequest
+
+export type AsyncFunction = (req: NextApiRequest, res: NextApiResponse) => Promise<any>
+
+export interface CatchError {
+  message: string
+  stack?: string
+}

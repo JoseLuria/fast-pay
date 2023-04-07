ed3dbe1944f3a27a96b851044480d8107d397465
JoseLuria <jos02-log18@hotmail.com>
2023-04-07 17:51:48 -0500

    feat: :passport_control: added next auth session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
 import type { FC } from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default App

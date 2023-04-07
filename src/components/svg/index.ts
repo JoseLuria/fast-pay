0b5a6cb267aa5432eeec5ab5d73565b988a905a8
JoseLuria <jos02-log18@hotmail.com>
2023-04-07 11:27:29 -0500

    feat: :lipstick: added svg icons and svgr config

---
+++ src/components/svg/index.ts
@@ -0,0 +1,6 @@
+export { default as ArrowDown } from './arrow-down.svg'
+export { default as Google } from './google.svg'
+export { default as Illustration } from './illustration.svg'
+export { default as Logo } from './logo.svg'
+export { default as Menu } from './menu.svg'
+export { default as Plus } from './plus.svg'

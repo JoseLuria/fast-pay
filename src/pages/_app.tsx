2caa5dd73ca90c8c51a9c7fcb1b5ddb7a53d42a1
JoseLuria <jos02-log18@hotmail.com>
2023-04-08 10:54:17 -0500

    feat: :lipstick: added axios interceptor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import '@/styles/globals.css'
 import type { FC } from 'react'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'sonner'
+import { AxiosInterceptor } from '@/utils'
+
+AxiosInterceptor()
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <SessionProvider>
       <Component {...pageProps} />
+      <Toaster richColors position='top-right' />
     </SessionProvider>
   )
 }

aa2b4910016335d8da4dfb5528ac11ce04ab12aa
JoseLuria <jos02-log18@hotmail.com>
2023-04-08 14:05:39 -0500

    refactor: :art: improved code structure

--- src/server/user.ts
+++ src/server/user.ts
@@ -5,15 +5,11 @@ import { Login } from '@/validations'
 export const authUser = async ({ email, password }: Login) => {
   const user = await db.user.findFirst({ where: { email } })
 
-  if (!user) {
-    return null
-  }
+  if (!user) return null
 
   const isValidPassword = bycript.compareSync(password, user.password)
 
-  if (!isValidPassword) {
-    return null
-  }
+  if (!isValidPassword) return null
 
   const { id, name, image, type } = user
 

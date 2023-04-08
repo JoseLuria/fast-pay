5775fd85182fdfa0229161f3346dc0c4aac18671
JoseLuria <jos02-log18@hotmail.com>
2023-04-08 16:18:54 -0500

    feat: :lipstick: styled app page

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,9 +1,21 @@
-import { DashboardLayout } from '@/components'
+import dynamic from 'next/dynamic'
+import { DashboardLayout, InvoiceHeader, InvoiceLoading } from '@/components'
+import { useInvoices } from '@/hooks'
+
+const InvoiceEmpty = dynamic(import('@/components').then(({ InvoiceEmpty }) => InvoiceEmpty))
+const InvoiceList = dynamic(import('@/components').then(({ InvoiceList }) => InvoiceList))
 
 const Dashboard = () => {
+  const { invoices, isLoading } = useInvoices()
+
   return (
-    <DashboardLayout title='Dashboard'>
-      <h1>Dashboard</h1>
+    <DashboardLayout title='Mis facturas'>
+      <InvoiceHeader size={invoices.length} />
+      {isLoading ? (
+        <InvoiceLoading />
+      ) : (
+        <>{invoices.length > 0 ? <InvoiceList invoices={invoices} /> : <InvoiceEmpty />}</>
+      )}
     </DashboardLayout>
   )
 }

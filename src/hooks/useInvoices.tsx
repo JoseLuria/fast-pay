b65f6dad0b0a6f25746e49ec665ca9ad2c988cbd
JoseLuria <jos02-log18@hotmail.com>
2023-04-08 22:42:57 -0500

    feat: :goal_net: added error handling

--- src/hooks/useInvoices.tsx
+++ src/hooks/useInvoices.tsx
@@ -1,18 +1,20 @@
+import type { SingleInvoice, Invoices } from '@/server/types'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import type { SingleInvoice, Invoices } from '@/server/types'
 
 export const useInvoices = () => {
   const [invoices, setInvoices] = useState<SingleInvoice[]>([])
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(false)
 
   const fetchInvoices = async () => {
+    setError(false)
     try {
       const { data } = await axios.get<Invoices>('/api/invoice')
       setInvoices(data.invoices)
       setIsLoading(false)
     } catch (error) {
-      setIsLoading(false)
+      setError(true)
     }
   }
 
@@ -20,5 +22,5 @@ export const useInvoices = () => {
     fetchInvoices()
   }, [])
 
-  return { isLoading, invoices }
+  return { isLoading, invoices, error }
 }

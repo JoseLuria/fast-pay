3a9dbaf036cf093c489a5eef8533e2e5fc1bedce
JoseLuria <jos02-log18@hotmail.com>
2023-04-09 14:18:49 -0500

    feat: :sparkles: created ticket and ticket form page

---
+++ src/pages/app/ticket/[id].tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '@/components'
+
+const TicketById = () => {
+  return (
+    <DashboardLayout title='Ticket'>
+      <h1>TicketById</h1>
+    </DashboardLayout>
+  )
+}
+
+export default TicketById
---
+++ src/pages/app/ticket/edit/[id].tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '@/components'
+
+const TicketForm = () => {
+  return (
+    <DashboardLayout title='Nuevo ticket'>
+      <h1>Ticket Form</h1>
+    </DashboardLayout>
+  )
+}
+
+export default TicketForm

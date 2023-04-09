444f2208bb6df285cc127c010706adb7a3741397
JoseLuria <jos02-log18@hotmail.com>
2023-04-09 14:18:33 -0500

    feat: :sparkles: created profile page

---
+++ src/pages/app/profile.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '@/components'
+
+const Profile = () => {
+  return (
+    <DashboardLayout title='Perfil'>
+      <h1>Perfil</h1>
+    </DashboardLayout>
+  )
+}
+
+export default Profile

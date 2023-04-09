158bb4bb8321963d8f53e8095227d0fc4ceb0d86
JoseLuria <jos02-log18@hotmail.com>
2023-04-09 14:51:54 -0500

    feat: :bento: replaced favicon assets

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,12 @@ const Document = () => {
   return (
     <Html lang='es'>
       <Head>
-        <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png' />
-        <link rel='manifest' href='site.webmanifest' />
-        <link rel='mask-icon' href='safari-pinned-tab.svg' color='#5bbad5' />
-        <meta name='msapplication-TileColor' content='#131313' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
         <meta name='theme-color' content='#131313' />
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'

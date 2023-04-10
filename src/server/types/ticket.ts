13d02fbba5cb5f8fe30aab7ba86dcace53aa4713
JoseLuria <jos02-log18@hotmail.com>
2023-04-09 22:51:00 -0500

    feat: :label: added single ticket type

--- src/server/types/ticket.ts
+++ src/server/types/ticket.ts
@@ -1,6 +1,7 @@
-import type { TicketSatus } from '@prisma/client'
+import type { TicketSatus, Ticket, Item } from '@prisma/client'
+import type { ApiError } from '@/server/types'
 
-export interface SingleTicket {
+export interface UserTicket {
   id: string
   date: Date
   clientName: string
@@ -8,6 +9,19 @@ export interface SingleTicket {
   total: number
 }
 
-export type Tickets = { tickets: SingleTicket[] }
+export type Tickets = { tickets: UserTicket[] }
 
-export type TicketApi = { message: string } | Tickets
+export interface SingleTicketItem extends Omit<Item, 'ticketId'> {
+  total: string
+}
+
+export interface SingleTicket extends Omit<Ticket, 'date' | 'terms' | 'userId' | 'transactionId'> {
+  createdAt: Date
+  expiredAt: Date
+  items: SingleTicketItem
+  total: number
+}
+
+export type TicketApi = ApiError | Tickets
+
+export type SingleTicketApi = ApiError | SingleTicket

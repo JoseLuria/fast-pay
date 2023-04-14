862706bea686203e134c59c5bc49eb0e65ccbd98
JoseLuria <jos02-log18@hotmail.com>
2023-04-14 14:22:54 -0500

    feat: :sparkles: added class name prop to dashboard layout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,19 +1,23 @@
 import type { FC, ReactNode } from 'react'
+import clsx from 'clsx'
 import { Head, Appbar } from '@/components'
 
 interface Props {
   title: string
   children: ReactNode
+  className?: string
 }
 
-export const DashboardLayout: FC<Props> = ({ title, children }) => {
+export const DashboardLayout: FC<Props> = ({ title, children, className }) => {
+  const styles = clsx('w-full max-w-container mx-auto', className)
+
   return (
     <>
       <Head title={title} />
       <Appbar />
 
       <div className='w-full mt-24 px-6 py-6 pb-12 md:px-12 md:pt-12 md:pb-24 lg:mt-0 lg:py-20 lg:ml-28 lg:w-auto lg:grow'>
-        <main className='w-full max-w-container mx-auto'>{children}</main>
+        <main className={styles}>{children}</main>
       </div>
     </>
   )

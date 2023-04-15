0a88ac36ec0e094bd2b35997d247308edd8ea067
JoseLuria <jos02-log18@hotmail.com>
2023-04-15 00:05:35 -0500

    fix: :bug: corrected calculate ticket total

--- src/server/utils/ticket.ts
+++ src/server/utils/ticket.ts
@@ -1,18 +1,15 @@
 import { Item } from '@prisma/client'
 
 export const calculateTicketTotal = (items: Item[]) => {
-  const { price: total } = items.reduce((prev, { price, quantity }) => ({
-    ...prev,
-    price: prev.price + price * quantity
-  }))
-
-  return total
+  return items.reduce((prevTotal, { price, quantity }) => {
+    const itemTotal = price * quantity
+    return prevTotal + itemTotal
+  }, 0)
 }
 
 export const calculateItemsTotal = (items: Item[]) => {
   return items.map(({ id, name, price, quantity }) => {
     const total = quantity * price
-
     return { id, name, price, quantity, total }
   })
 }

ec6fe3ca4a436dd65c7c67a1ceffc9d2c813ca54
JoseLuria <jos02-log18@hotmail.com>
2023-04-15 00:04:33 -0500

    feat: :sparkles: created ticket data constant

---
+++ src/constants/ticket.ts
@@ -0,0 +1,17 @@
+export const statusData = {
+  paid: {
+    text: 'Pagado',
+    color: 'bg-green',
+    colorText: 'text-green'
+  },
+  notPaid: {
+    text: 'Pendiente',
+    color: 'bg-orange',
+    colorText: 'text-orange'
+  },
+  paused: {
+    text: 'Pausado',
+    color: 'bg-red',
+    colorText: 'text-red'
+  }
+}

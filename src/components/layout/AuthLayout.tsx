318c99a59f174877ac6e0ec7c7b5cee811894397
JoseLuria <jos02-log18@hotmail.com>
2023-04-26 16:12:08 -0500

    feat: :lipstick: added home link to auth layout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode, FC } from 'react'
+import Link from 'next/link'
 import { Head } from '@/components'
+import { Logo } from '@/components/svg'
 
 interface Props {
   title: string
@@ -10,7 +12,13 @@ export const AuthLayout: FC<Props> = ({ title, children }) => {
   return (
     <>
       <Head title={title} />
-      <main className='w-full px-6 flex flex-col py-20 items-center md:px-12'>{children}</main>
+      <main className='w-full px-6 flex flex-col py-20 items-center md:px-12'>
+        <Link className='mb-10' href='/' title='Inicio'>
+          <Logo height={40} />
+        </Link>
+
+        {children}
+      </main>
     </>
   )
 }

445fae4d8f67d77ce4db635dc570d25b8364d4b9
JoseLuria <jos02-log18@hotmail.com>
2023-05-09 14:44:49 -0500

    feat: :sparkles: added get ticket data function

--- src/server/ticket.ts
+++ src/server/ticket.ts
@@ -1,4 +1,5 @@
 import { db, calculateTicketTotal } from '@/server'
+import { AppError, calculateItemsTotal, excludeFields } from '@/server/utils'
 
 export const getUserTickets = async (userId: string) => {
   const dbTickets = await db.ticket.findMany({
@@ -16,4 +17,27 @@ export const getUserTickets = async (userId: string) => {
     const total = calculateTicketTotal(items)
     return { id, date, clientName, status, total }
   })
+}
+
+export const getTicketData = async ({ userId, id }: { userId: string; id: string }) => {
+  try {
+    const dbTicket = await db.ticket.findFirst({
+      where: { id, userId },
+      include: {
+        items: true
+      }
+    })
+
+    if (!dbTicket) return
+
+    const { items: ticketItems, ...data } = excludeFields(dbTicket, ['transactionId', 'userId'])
+
+    const items = calculateItemsTotal(ticketItems)
+    const total = calculateTicketTotal(ticketItems)
+
+    return { ...data, items, total }
+  } catch (error) {
+    const { message } = error as Error
+    throw new AppError(500, message)
+  }
 }

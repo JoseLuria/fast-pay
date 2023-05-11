6fa4fb8af55a463b3b41ca25df10f02e735ea8c5
JoseLuria <jos02-log18@hotmail.com>
2023-05-11 09:37:17 -0500

    feat: :lipstick: created ticket details component

--- src/components/ticket/TicketDetailsPlaceholder.tsx
+++ src/components/ticket/TicketDetailsPlaceholder.tsx
@@ -1,7 +1,7 @@
 export const TicketDetailsPlaceholder = () => {
   return (
     <>
-      <section className='bg-dark-gray h-20 rounded-md w-full mt-8 mb-6 animate-pulse' />
+      <section className='bg-dark-gray h-20 rounded-md w-full mb-6 animate-pulse' />
       <section className='bg-dark-gray h-96 rounded-md w-full animate-pulse' />
     </>
   )

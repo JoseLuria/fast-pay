c4a1c875dfbaef757a8a0125ad580fced6276f0b
JoseLuria <jos02-log18@hotmail.com>
2023-05-11 09:38:07 -0500

    feat: :sparkles: added get ticket details by id

--- src/pages/app/ticket/[id].tsx
+++ src/pages/app/ticket/[id].tsx
@@ -1,14 +1,33 @@
+import type { FC } from 'react'
+import type { SingleTicket } from '@/server/types'
+import { GetServerSideProps } from 'next'
 import { DashboardLayout, BackButton, TicketDetailsPlaceholder, ErrorBoundary } from '@/components'
+import { useFetch } from '@/hooks'
+import dynamic from 'next/dynamic'
 
-const TicketById = () => {
+const TicketDetails = dynamic(import('@/components').then(({ TicketDetails }) => TicketDetails))
+
+interface Props {
+  id: string
+}
+
+const TicketById: FC<Props> = ({ id }) => {
+  const { data, status } = useFetch<SingleTicket>(`/api/ticket/${id}`)
+
   return (
-    <ErrorBoundary>
-      <DashboardLayout className='mb-24 lg:mb-0' title='Ticket'>
-        <BackButton />
-        <TicketDetailsPlaceholder />
+    <ErrorBoundary type='page' error={status === 'error'}>
+      <DashboardLayout className='mb-24 lg:mb-0 text-sm' title='Ticket'>
+        <BackButton className='mb-8' />
+        {status === 'loaded' && data ? <TicketDetails {...data} /> : <TicketDetailsPlaceholder />}
       </DashboardLayout>
     </ErrorBoundary>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id } = query as { id: string }
+
+  return { props: { id } }
 }
 
 export default TicketById
